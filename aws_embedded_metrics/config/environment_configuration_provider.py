322850bb5e846355d8db0de57b8b4998a825af67
Jared Nance <jarnance@amazon.com>
2019-10-25 12:57:45 -0700

    add environment configuration

---
+++ aws_embedded_metrics/config/environment_configuration_provider.py
@@ -0,0 +1,44 @@
+import os
+from typing import TypeVar
+from aws_embedded_metrics.config.configuration import Configuration
+
+ENV_VAR_PREFIX = "AWS_EMF"
+
+ENABLE_DEBUG_LOGGING = "ENABLE_DEBUG_LOGGING"
+SERVICE_NAME = "SERVICE_NAME"
+SERVICE_TYPE = "SERVICE_TYPE"
+LOG_GROUP_NAME = "LOG_GROUP_NAME"
+LOG_STREAM_NAME = "LOG_STREAM_NAME"
+AGENT_ENDPOINT = "AGENT_ENDPOINT"
+
+T = TypeVar("T")
+
+
+class EnvironmentConfigurationProvider:
+    """
+    Loads configuration from environment variables
+    """
+
+    def get_configuration(self) -> Configuration:
+        return Configuration(
+            self.__get_bool_env_var(ENABLE_DEBUG_LOGGING),
+            self.__get_env_var(SERVICE_NAME),
+            self.__get_env_var(SERVICE_TYPE),
+            self.__get_env_var(LOG_GROUP_NAME),
+            self.__get_env_var(LOG_STREAM_NAME),
+            self.__get_env_var(AGENT_ENDPOINT),
+        )
+
+    @staticmethod
+    def __get_env_var(key: str) -> str:
+        value = os.environ.get(f"{ENV_VAR_PREFIX}_{key}")
+        if value is None:
+            return ""
+        return value
+
+    @staticmethod
+    def __get_bool_env_var(key: str) -> bool:
+        value = os.environ.get(f"{ENV_VAR_PREFIX}_{key}")
+        if value is None:
+            return False
+        return value.lower() == "true"
---
+++ tests/config/test_config.py
@@ -0,0 +1,88 @@
+from aws_embedded_metrics import config
+from faker import Faker
+import os
+from importlib import reload
+import pytest
+
+
+fake = Faker()
+
+
+@pytest.fixture
+def before(self):
+    os.environ["AWS_EMF_ENABLE_DEBUG_LOGGING"] = None
+    os.environ["AWS_EMF_SERVICE_NAME"] = None
+    os.environ["AWS_EMF_SERVICE_TYPE"] = None
+    os.environ["AWS_EMF_LOG_GROUP_NAME"] = None
+    os.environ["AWS_EMF_LOG_STREAM_NAME"] = None
+    os.environ["AWS_EMF_AGENT_ENDPOINT"] = None
+
+
+def get_config():
+    # reload the configuration module since it is loaded on
+    # startup and cached
+    reload(config)
+    return config.get_config()
+
+
+def test_can_get_config_from_environment():
+    # arrange
+    debug_enabled = True
+    service_name = fake.word()
+    service_type = fake.word()
+    log_group = fake.word()
+    log_stream = fake.word()
+    agent_endpoint = fake.word()
+
+    os.environ["AWS_EMF_ENABLE_DEBUG_LOGGING"] = str(debug_enabled)
+    os.environ["AWS_EMF_SERVICE_NAME"] = service_name
+    os.environ["AWS_EMF_SERVICE_TYPE"] = service_type
+    os.environ["AWS_EMF_LOG_GROUP_NAME"] = log_group
+    os.environ["AWS_EMF_LOG_STREAM_NAME"] = log_stream
+    os.environ["AWS_EMF_AGENT_ENDPOINT"] = agent_endpoint
+
+    # act
+    result = get_config()
+
+    # assert
+    assert result.debug_logging_enabled == debug_enabled
+    assert result.service_name == service_name
+    assert result.service_type == service_type
+    assert result.log_group_name == log_group
+    assert result.log_stream_name == log_stream
+    assert result.agent_endpoint == agent_endpoint
+
+
+def test_can_override_config():
+    # arrange
+    os.environ["AWS_EMF_ENABLE_DEBUG_LOGGING"] = str(True)
+    os.environ["AWS_EMF_SERVICE_NAME"] = fake.word()
+    os.environ["AWS_EMF_SERVICE_TYPE"] = fake.word()
+    os.environ["AWS_EMF_LOG_GROUP_NAME"] = fake.word()
+    os.environ["AWS_EMF_LOG_STREAM_NAME"] = fake.word()
+    os.environ["AWS_EMF_AGENT_ENDPOINT"] = fake.word()
+
+    config = get_config()
+
+    debug_enabled = False
+    service_name = fake.word()
+    service_type = fake.word()
+    log_group = fake.word()
+    log_stream = fake.word()
+    agent_endpoint = fake.word()
+
+    # act
+    config.debug_logging_enabled = debug_enabled
+    config.service_name = service_name
+    config.service_type = service_type
+    config.log_group_name = log_group
+    config.log_stream_name = log_stream
+    config.agent_endpoint = agent_endpoint
+
+    # assert
+    assert config.debug_logging_enabled == debug_enabled
+    assert config.service_name == service_name
+    assert config.service_type == service_type
+    assert config.log_group_name == log_group
+    assert config.log_stream_name == log_stream
+    assert config.agent_endpoint == agent_endpoint
