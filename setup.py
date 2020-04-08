39d367027f6bf450c182cd6f3561f1257da1509f
Jared Nance <jaredcnance@gmail.com>
2020-04-08 06:39:10 -0700

    v1.0.1 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="aws-embedded-metrics",
-    version="1.0.1b4",
+    version="1.0.1",
     author="Amazon Web Services",
     author_email="jarnance@amazon.com",
     description="AWS Embedded Metrics Package",
