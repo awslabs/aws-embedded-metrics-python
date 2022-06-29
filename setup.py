7f6cb1ca7aa14cc583d70a4658a3679dc8fec3cc
Himtanaya Bhadada <himmy@amazon.com>
2022-06-29 10:24:03 -0700

    Bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="aws-embedded-metrics",
-    version="1.0.8",
+    version="2.0.0",
     author="Amazon Web Services",
     author_email="jarnance@amazon.com",
     description="AWS Embedded Metrics Package",
