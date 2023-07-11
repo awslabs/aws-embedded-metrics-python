0c447f9489b317866f143275cec04d5f4719b70d
Himtanaya Bhadada <himtanaya@gmail.com>
2023-07-11 13:06:26 -0700

    bump version to 3.1.1 (#107)
    * bump version to 3.1.1
    
    * Reverting to old stable debain version to let the build pass
    
    ---------
    
    Co-authored-by: Himtanaya Bhadada <himmy@amazon.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="aws-embedded-metrics",
-    version="3.1.0",
+    version="3.1.1",
     author="Amazon Web Services",
     author_email="jarnance@amazon.com",
     description="AWS Embedded Metrics Package",
