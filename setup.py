9dcff13f247a8b409a89c1bafa3a4a9caa288a4d
seoberha <seoberha@amazon.com>
2025-02-05 11:34:30 -0800

    Bump version to 3.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="aws-embedded-metrics",
-    version="3.2.0",
+    version="3.3.0",
     author="Amazon Web Services",
     author_email="jarnance@amazon.com",
     description="AWS Embedded Metrics Package",
